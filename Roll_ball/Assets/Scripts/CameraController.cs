53932ae865717811e38e38be32eaf899efb563f4
Anant Sharma <ananttheant4444@gmail.com>
2017-10-09 19:13:13 +0530

    3rd person and movement added
    (via script 3rd p)

---
+++ Roll_ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject PlayerGameObject;
+    private Vector3 _offset;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _offset = GetComponent<Transform>().position - PlayerGameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+	    transform.position = PlayerGameObject.transform.position + _offset;
+	}
+}
