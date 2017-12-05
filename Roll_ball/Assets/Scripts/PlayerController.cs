d634bbc3e7b5a2e6b684621f682cb0451b027fe3
Anant Sharma <ananttheant4444@gmail.com>
2017-12-05 23:45:07 +0530

    Revert "UI (counting and Win text)"
    This reverts commit 3a5906a4d0f6341e093dcc3d8348666ee8686fc3.

--- Roll_ball/Assets/Scripts/PlayerController.cs
+++ Roll_ball/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed;
-    public Text CountText;
-    public Text WinText;
-
     private Rigidbody _player;
-    private int _count;
 
     void Start()
     {
         _player = GetComponent<Rigidbody>();
-        _count = 0;
-        SetCountText();
-        WinText.text = "";
     }
 
     void FixedUpdate()
@@ -36,17 +28,6 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
-            _count = _count + 1;
-            SetCountText();
-        }
-    }
-
-    void SetCountText()
-    {
-        CountText.text = "Count: " + _count.ToString();
-        if (_count >= 16)
-        {
-            WinText.text = "You Win!";
         }
     }
 
